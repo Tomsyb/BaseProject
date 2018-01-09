85c61d638dcd5cce24c3767e054a831b67d93024
Tomasyb <760375443@qq.com>
2018-01-09 23:18:54 +0800

    dagger2首页实现列表

--- app/src/main/java/com/android/daqsoft/baseapi/dagger2/ui/DaggerMainActivity.java
+++ app/src/main/java/com/android/daqsoft/baseapi/dagger2/ui/DaggerMainActivity.java
@@ -3,8 +3,14 @@ package com.android.daqsoft.baseapi.dagger2.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import com.android.daqsoft.adapter.recyclerview.CommonAdapter;
+import com.android.daqsoft.adapter.recyclerview.base.ViewHolder;
 import com.android.daqsoft.baseproject.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,13 +23,34 @@ public class DaggerMainActivity extends AppCompatActivity {
     @BindView(R.id.dagger_main_rv)
     RecyclerView mRv;
 
-
+    List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dagger2_main);
         ButterKnife.bind(this);
+        initDatas();
+        mRv.setAdapter(new CommonAdapter<String>(this,R.layout.item_common_text,mDatas) {
 
+            @Override
+            protected void convert(ViewHolder holder, String s, int position) {
+                holder.setText(R.id.item_tv,s);
+                holder.setOnClickListener(R.id.item_ll, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        String[] stringArray = getResources().getStringArray(R.array.dagger2mainlist);
+        for (int i = 0; i < stringArray.length; i++) {
+            mDatas.add(stringArray[i]);
+        }
     }
 }
