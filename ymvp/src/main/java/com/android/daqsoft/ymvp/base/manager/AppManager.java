88de4e1588f73bbded8784058887feb3cdbfad16
Tomasyb <760375443@qq.com>
2018-01-10 23:53:29 +0800

    框架搭建AppmanagerActivity的kill

--- ymvp/src/main/java/com/android/daqsoft/ymvp/base/manager/AppManager.java
+++ ymvp/src/main/java/com/android/daqsoft/ymvp/base/manager/AppManager.java
@@ -1,16 +1,33 @@
 package com.android.daqsoft.ymvp.base.manager;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.os.Message;
 
 import org.simple.eventbus.EventBus;
 import org.simple.eventbus.Subscriber;
 import org.simple.eventbus.ThreadMode;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.inject.Singleton;
+
 /**
  * Created by yanbo on 2018/1/10.
  * 用于管理所有Activity类
  * 也可通过post(Message) 远程遥控执行对应的方法，用法和EventBus类似
+ * API:
+ *      appExit()        退出程序
+ *      getActivityList()获取所有存活的Activity
+ *      killAll()        杀掉所有Activity 通过Iterator实现
+ *      KillAllStillOne()杀死所有Activity保留一个
+ *
+ *
+ *
  *
  */
 
@@ -28,6 +45,11 @@ public class AppManager {
     public static final int EXIT_APP = 5002;
 
     /**
+     * 管理所有的Activity,容器中的Activity是Activity的创建顺序，不能保证和Activity的任务栈一致
+     */
+    public List<Activity> mActivityList;
+
+    /**
      * 构造方法
      *  获取application来操作页面的添加与跳转
      *  远程操控
@@ -43,7 +65,7 @@ public class AppManager {
     public void onReceive(Message msg){
         switch (msg.what){
             case START_ACTIVITY:
-
+                dispathStart(msg);
                 break;
             case KILLALL_ACTIVITY:
                 break;
@@ -53,4 +75,82 @@ public class AppManager {
                 break;
         }
     }
+
+    /**
+     * @param msg 根据类型判断跳转
+     */
+    private void dispathStart(Message msg) {
+        if (msg.obj instanceof Intent);
+
+    }
+
+    /**
+     * 让栈顶的Activity打开指定的Activity
+     * @param intent
+     */
+    public void startActivity(Intent intent){
+
+    }
+    /**
+     * 获取所有存活的activity
+     */
+    public List<Activity> getActivityList(){
+        if (mActivityList == null){
+            mActivityList = new LinkedList<>();
+        }
+        return mActivityList;
+    }
+    /**
+     * 杀掉所有Activity
+     */
+    public void killAll(){
+//        while (getActivityList().size() != 0) { //此方法只能兼容LinkedList
+//            getActivityList().remove(0).finish();
+//        }
+        synchronized (AppManager.class){
+            Iterator<Activity> iterator = getActivityList().iterator();
+            while (iterator.hasNext()){
+                Activity next = iterator.next();
+                iterator.remove();
+                next.finish();
+            }
+        }
+    }
+    /**
+     * 杀死所有Activity保留一个
+     */
+    public void killAllStillOne(Class<?>... excludeActivityClasses){
+        List<Class<?>> encludeList = Arrays.asList(excludeActivityClasses);
+        synchronized (AppManager.class){
+            Iterator<Activity> iterator = getActivityList().iterator();
+            while (iterator.hasNext()){
+                Activity next = iterator.next();
+                if (encludeList.contains(next.getClass()))
+                    continue;
+                iterator.remove();
+                next.finish();
+
+            }
+        }
+    }
+
+    /**
+     * 退出程序
+     */
+    public void appExit(){
+        try {
+            //killAll();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 接口
+     */
+    public interface HandleListener{
+        void handleMessage(AppManager appManager,Message message);
+    }
+
 }
